b3c1bb4ae06e8864bed6cef74358a09c750da16f
Brayden O'Neal <braydenioneal@gmail.com>
2026-02-11 16:13:05 -0600

    Implemented more accurate rotation

--- src/tetris/piece.py
+++ src/tetris/piece.py
@@ -11,6 +11,25 @@ class Piece:
         self.y = 0
         self.rotations = 0  # 1-3
 
+    def _local_tiles(self):
+        tiles = self.shape.tiles
+
+        for _ in range(self.rotations):
+            tiles = [(-y, x) for x, y in tiles]
+
+        min_x = min(x for x, _ in tiles)
+        min_y = min(y for _, y in tiles)
+
+        return [(x - min_x, y - min_y) for x, y in tiles]
+
+    def _center(self):
+        tiles = self._local_tiles()
+
+        max_x = max(x for x, _ in tiles) + 1
+        max_y = max(y for _, y in tiles) + 1
+
+        return max_x // 2, max_y // 2
+
     def move_left(self):
         self.x -= 1
 
@@ -18,20 +37,16 @@ class Piece:
         self.x += 1
 
     def rotate_cw(self):
+        prev_center_x, prev_center_y = self._center()
+
         self.rotations = (self.rotations + 1) % 4
+        center_x, center_y = self._center()
+
+        self.x += prev_center_x - center_x
+        self.y += prev_center_y - center_y
 
     def rotate_ccw(self):
         self.rotations = (self.rotations - 1) % 4
 
     def tiles(self):
-        tiles = self.shape.tiles
-
-        for _ in range(self.rotations):
-            tiles = [(-y, x) for x, y in tiles]
-
-        min_x = min(x for x, y in tiles)
-        min_y = min(y for x, y in tiles)
-
-        tiles = [(x - min_x, y - min_y) for x, y in tiles]
-
-        return [(x + self.x, y + self.y) for x, y, in tiles]
+        return [(x + self.x, y + self.y) for x, y, in self._local_tiles()]
