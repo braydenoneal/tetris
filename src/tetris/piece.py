beff42ac7d13f4d990d588e1eb2e50b8f662afb9
Brayden O'Neal <braydenioneal@gmail.com>
2026-02-13 16:12:12 -0600

    Implement accurate rotation orientations

--- src/tetris/piece.py
+++ src/tetris/piece.py
@@ -11,25 +11,6 @@ class Piece:
         self.y = 0
         self.rotations = 0  # 1-3
 
-    def _local_tiles(self):
-        tiles = self.shape.tiles
-
-        for _ in range(self.rotations):
-            tiles = [(-y, x) for x, y in tiles]
-
-        min_x = min(x for x, _ in tiles)
-        min_y = min(y for _, y in tiles)
-
-        return [(x - min_x, y - min_y) for x, y in tiles]
-
-    def _center(self):
-        tiles = self._local_tiles()
-
-        max_x = max(x for x, _ in tiles) + 1
-        max_y = max(y for _, y in tiles) + 1
-
-        return max_x // 2, max_y // 2
-
     def move_left(self):
         self.x -= 1
 
@@ -37,16 +18,18 @@ class Piece:
         self.x += 1
 
     def rotate_cw(self):
-        prev_center_x, prev_center_y = self._center()
-
         self.rotations = (self.rotations + 1) % 4
-        center_x, center_y = self._center()
-
-        self.x += prev_center_x - center_x
-        self.y += prev_center_y - center_y
 
     def rotate_ccw(self):
         self.rotations = (self.rotations - 1) % 4
 
     def tiles(self):
-        return [(x + self.x, y + self.y) for x, y, in self._local_tiles()]
+        tiles = self.shape.tiles
+        center_x, center_y = self.shape.center
+
+        for _ in range(self.rotations):
+            tiles = [(x - center_x, y - center_y) for x, y in tiles]
+            tiles = [(-y, x) for x, y in tiles]
+            tiles = [(round(x + center_x), round(y + center_y)) for x, y in tiles]
+
+        return [(x + self.x, y + self.y) for x, y, in tiles]
--- src/tetris/shape.py
+++ src/tetris/shape.py
@@ -6,16 +6,17 @@ class Shape:
     name: str
     color: int
     tiles: list[tuple[int, int]]
+    center: tuple[float, float]
 
 
 SHAPES: list[Shape] = [
-    Shape("I", 0x00FFFF, [(0, 0), (1, 0), (2, 0), (3, 0)]),  # Cyan
-    Shape("O", 0xFFFF00, [(0, 0), (1, 0), (0, 1), (1, 1)]),  # Yellow
-    Shape("T", 0xFF00FF, [(0, 0), (1, 0), (2, 0), (1, 1)]),  # Purple
-    Shape("S", 0x00FF00, [(1, 0), (2, 0), (0, 1), (1, 1)]),  # Green
-    Shape("Z", 0xFF0000, [(0, 0), (1, 0), (1, 1), (2, 1)]),  # Red
-    Shape("J", 0x0000FF, [(0, 0), (1, 0), (2, 0), (2, 1)]),  # Blue
-    Shape("L", 0xFF7F00, [(0, 0), (1, 0), (2, 0), (0, 1)]),  # Orange
+    Shape("I", 0x00FFFF, [(0, 0), (1, 0), (2, 0), (3, 0)], (1.5, 0.5)),  # Cyan
+    Shape("O", 0xFFFF00, [(0, 0), (1, 0), (0, 1), (1, 1)], (0.5, 0.5)),  # Yellow
+    Shape("T", 0xFF00FF, [(0, 0), (1, 0), (2, 0), (1, 1)], (1, 0)),  # Purple
+    Shape("S", 0x00FF00, [(1, 0), (2, 0), (0, 1), (1, 1)], (1, 0)),  # Green
+    Shape("Z", 0xFF0000, [(0, 0), (1, 0), (1, 1), (2, 1)], (1, 0)),  # Red
+    Shape("J", 0x0000FF, [(0, 0), (1, 0), (2, 0), (2, 1)], (1, 0)),  # Blue
+    Shape("L", 0xFF7F00, [(0, 0), (1, 0), (2, 0), (0, 1)], (1, 0)),  # Orange
 ]
 
 
